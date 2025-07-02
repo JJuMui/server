2e0a56ca0e7a31a336893094877e1309555c0ab1
wonjuneee <tony000709@gmail.com>
2025-07-02 20:17:36 +0900

    Initial Commit

---
+++ src/main/java/com/jjumui/eodiroo/EodirooApplication.java
@@ -0,0 +1,13 @@
+package com.jjumui.eodiroo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EodirooApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EodirooApplication.class, args);
+    }
+
+}
